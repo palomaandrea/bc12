d516f164b72740d7ff9c70ab2e6a0d1184648356
Poipurin <b.pinorojo@gmail.com>
2023-05-03 15:11:55 -0400

    se añade Registro pasajero a pages

---
+++ src/test/java/aut/testcreation/pages/rumboesabbaro/RegistroPasajeroPage.java
@@ -0,0 +1,10 @@
+package aut.testcreation.pages.rumboesabbaro;
+
+import framework.engine.selenium.SeleniumWrapper;
+import org.openqa.selenium.WebDriver;
+
+public class RegistroPasajeroPage extends SeleniumWrapper {
+    public RegistroPasajeroPage(WebDriver driver) {
+        super(driver);
+    }
+}
