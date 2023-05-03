86447b797bc26c23de09d12f908df8e63f7d15d9
Poipurin <b.pinorojo@gmail.com>
2023-05-03 12:47:45 -0400

    se añaden locators a VuelosPage

--- src/test/java/aut/testcreation/pages/rumboesabbaro/VuelosPage.java
+++ src/test/java/aut/testcreation/pages/rumboesabbaro/VuelosPage.java
@@ -1,10 +1,21 @@
 package aut.testcreation.pages.rumboesabbaro;
 
 import framework.engine.selenium.SeleniumWrapper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class VuelosPage extends SeleniumWrapper {
     public VuelosPage(WebDriver driver) {
         super(driver);
     }
+
+    By locatorSelectorSentidoViaje = By.xpath("");
+    By locatorTxtIda = By.xpath("");
+    By locatorTxtvuelta = By.xpath("");
+    By locatorSelectorFechaIda = By.xpath("");
+    By locatorSelectorFechaVuelta = By.xpath("");
+    By locatorSelectorPasajeros = By.xpath("");
+    By locatorSelectorMetodoPago = By.xpath("");
+    By locatorBtnBuscar = By.xpath("");
+    By locatorTxtEuropa = By.xpath("");
 }
