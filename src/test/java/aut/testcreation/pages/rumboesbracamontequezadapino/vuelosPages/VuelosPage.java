12cc6fa1e025ce6784b666ab13a926884fdb44e9
Poipurin <b.pinorojo@gmail.com>
2023-05-04 12:06:36 -0400

    se añaden métodos a VuelosPage

--- src/test/java/aut/testcreation/pages/rumboesbracamontequezadapino/vuelosPages/VuelosPage.java
+++ src/test/java/aut/testcreation/pages/rumboesbracamontequezadapino/vuelosPages/VuelosPage.java
@@ -2,7 +2,10 @@ package aut.testcreation.pages.rumboesbracamontequezadapino.vuelosPages;
 
 import framework.engine.selenium.SeleniumWrapper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+
+import static org.openqa.selenium.Keys.ENTER;
 
 public class VuelosPage extends SeleniumWrapper {
     public VuelosPage(WebDriver driver) {
@@ -17,7 +20,48 @@ public class VuelosPage extends SeleniumWrapper {
     By locatorSelectIda = By.xpath("");
     By locatorSelectVuelta = By.xpath("");
     By locatorSelectorPasajeros = By.xpath("//span[@class='d-5n2gua']");
+    By locatorSelectorAnadirAdulto = By.xpath("");
     By locatorSelectorMetodoPago = By.xpath("//span[@class='d-ocxq5f']");
+    By locatorSelectorPagoMastercard = By.xpath("//li[contains(text(),'Mastercard')]");
+    By locatorSelectorPagoEconomico = By.xpath("//li[contains(text(),'económico')]");
     By locatorBtnBuscar = By.xpath("//button[@type='submit']");
     By locatorTxtEuropa = By.xpath("//h4[contains(text(),'Vuela a Europa')]");
+
+    public void seleccionarSentidoViajeIdaYVuelta(){
+        click(locatorSelectorSentidoViaje);
+    }
+    public void ingresarOrigen (String origen){
+        write(origen, locatorInputOrigen);
+        sendKeys(ENTER, locatorInputOrigen);
+    }
+    public void ingresarDestino (String destino){
+        write(destino, locatorInputDestino);
+        sendKeys(ENTER, locatorInputDestino);
+    }
+
+    public void seleccionarFecha (){
+        click(locatorSelectorFecha);
+        click(locatorSelectMes);
+        click(locatorSelectIda);
+        click(locatorSelectVuelta);
+    }
+    public void seleccionarAnadirPasajerosAdultos(){
+        click(locatorSelectorPasajeros);
+        click(locatorSelectorAnadirAdulto);
+    }
+    public void seleccionarMetodoPagoMastercard(){
+        click(locatorSelectorMetodoPago);
+        click(locatorSelectorPagoMastercard);
+    }
+    public void seleccionarMetodoPagoEconomico(){
+        click(locatorSelectorMetodoPago);
+        click(locatorSelectorPagoEconomico);
+    }
+
+    public  void buscarVuelo(){
+        click(locatorBtnBuscar);
+    }
+    public void vuelaAEuropa(){
+        click(locatorTxtEuropa);
+    }
 }
