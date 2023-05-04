bf613e445ed7b5606b3e77b8aa507748a53e7aed
Poipurin <b.pinorojo@gmail.com>
2023-05-04 13:18:27 -0400

    se crea base de test para vuelos

---
+++ src/test/java/aut/testcreation/testcases/rumboesbracamontequezadapino/VuelosTest.java
@@ -0,0 +1,22 @@
+package aut.testcreation.testcases.rumboesbracamontequezadapino;
+import aut.testcreation.pages.rumboesbracamontequezadapino.HomePage;
+import aut.testcreation.pages.rumboesbracamontequezadapino.vuelosPages.VuelosBusquedaPage;
+import aut.testcreation.pages.rumboesbracamontequezadapino.vuelosPages.VuelosPage;
+import framework.engine.selenium.DriverFactory;
+import framework.engine.selenium.SeleniumTestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+public class VuelosTest extends SeleniumTestBase {
+
+    HomePage homepage;
+    VuelosPage vuelosPage;
+    VuelosBusquedaPage vuelosBusquedaPage;
+
+    @Test
+    public void CP001_BusquedaVuelo_IdaYVuelta_MasBarato_OK(){
+
+    }
+
+
+}
