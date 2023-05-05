809741a480acf4e88b669760c174af94cca9387f
Poipurin <b.pinorojo@gmail.com>
2023-05-05 16:59:40 -0400

    se añaden xpath y métodos para vuelo en FlashSalesPage

--- src/test/java/aut/testcreation/pages/rumboesbracamontequezadapino/FlashSalesPage.java
+++ src/test/java/aut/testcreation/pages/rumboesbracamontequezadapino/FlashSalesPage.java
@@ -1,10 +1,23 @@
 package aut.testcreation.pages.rumboesbracamontequezadapino;
 
 import framework.engine.selenium.SeleniumWrapper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlashSalesPage extends SeleniumWrapper {
     public FlashSalesPage(WebDriver driver) {
         super(driver);
+    }
+    By locatorTxtOfertasDeLaSemana = By.xpath("//span[contains(text(),'Ofertas de la semana')]");
+    By locatorSelectOferta = By.xpath("//div[@class='slick-list']");
+
+    public void ofertaVuelo(){
+        scroll(findElement(locatorTxtOfertasDeLaSemana));
+        List<WebElement> ofertasSemana = findElements(locatorSelectOferta);
+        click(ofertasSemana.get(1));
     }
 }
