97e32753dc16352c7e5ec9b54d4523a04807cd75
loudelmar <lourdeslede17@gmail.com>
2023-05-09 15:25:23 -0300

    Test 3 Vuelos listo y funcionando

--- src/test/java/aut/testcreation/pages/grupo1/VuelosPage.java
+++ src/test/java/aut/testcreation/pages/grupo1/VuelosPage.java
@@ -16,9 +16,10 @@ public class VuelosPage extends SeleniumBase{
     By btnDiaIda=By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[4]/div[1]/div/div[2]/div/div/section/div/div/div[2]/div[2]/button[1]");
     By btnDiaVuelta=By.xpath("//*[@id=\"hub-csw-container\"]/div/div/form/div[2]/div[2]/div[1]/div/div/div/div/section/div/div/div[2]/div[2]/button[2]");
     By btnPasajeros=By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[5]/div/div/div/button");
-    By btnAgregarPasajeros=By.xpath("//*[@id='hub-csw-container']/div/div/form/div[2]/div[3]/div[1]/div/div[2]/div/section/div/div[1]/div[2]/button[2]");
+    By btnAgregarPasajeros=By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[5]/div[1]/div/div[2]/div/section/div/div[1]/div[2]/button[2]");
     By btnBuscar=By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[6]/div/button");
     By txtBusqueda=By.xpath("//*[@id=\"scrollableHeader\"]/div/div/div/div[1]/div/div/div/div/div[2]/div/div[1]");
+    By txtBusquedaPestania=By.xpath("//*[@id=\"scrollableHeader\"]/div/div/div/div[1]/div/div/div/div/div[2]/div/div[1]");
     By btnSoloIda=By.xpath("//*[@id=\"hub-csw-container\"]/div/div[2]/div/form/div/div[1]/div/div/div[2]");
     public void EscribirOrigen() throws InterruptedException {
         //List<WebElement> origenes = buscarElementosWeb(btnOrigen);
@@ -37,6 +38,14 @@ public class VuelosPage extends SeleniumBase{
         Thread.sleep(2000);
         escribir(Keys.TAB,btnDestino);
     }
+    public void EscribirDestinoEquivocado() throws InterruptedException {
+        //List<WebElement> origenes = buscarElementosWeb(btnOrigen);
+        clickear(btnDestino);
+        Thread.sleep(3000);
+        escribir("Barcelona (BCN) - Todos los aeropuertos, España",btnDestino);
+        Thread.sleep(2000);
+        escribir(Keys.TAB,btnDestino);
+    }
     public void ClickDia(){
         clickear(btnDiaIda);
     }
@@ -52,13 +61,21 @@ public class VuelosPage extends SeleniumBase{
     public String obtenerTextoBusqueda(){
         return obtenerTexto(txtBusqueda);
     }
-    public void AgregarPasajeros() throws InterruptedException {
+    public String obtenerTextoBusquedaPestania(){
+        return obtenerTexto(txtBusquedaPestania);
+    }
+    public void AgregarPasajero() throws InterruptedException {
         clickear(btnPasajeros);
         Thread.sleep(3000);
+    }
+    public void AgregarPasajeros() throws InterruptedException{
         clickear(btnAgregarPasajeros);
     }
     public void ClickSoloIda(){
         clickear(btnSoloIda);
+    }
+    public void CambiarPestaniaResultados(){
+        switchPestaniaPorTitulo("Rumbo vuelos baratos La Rioja");
     }
 
 }
--- src/test/java/aut/testcreation/testcases/grupo1/TestsVuelos.java
+++ src/test/java/aut/testcreation/testcases/grupo1/TestsVuelos.java
@@ -19,7 +19,7 @@ public class TestsVuelos extends TestBase {
         homePage.navegarAPagina("https://www.rumbo.es/");
         homePage.aceptarCookies();
         homePage.clickVuelos();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
         vuelosPage.ClickSoloIda();
         vuelosPage.EscribirOrigen();
         vuelosPage.EscribirDestino();
@@ -27,5 +27,47 @@ public class TestsVuelos extends TestBase {
         vuelosPage.ClickPasajeros();
         vuelosPage.ClickBuscar();
         Assertions.assertEquals(vuelosPage.obtenerTextoBusqueda(), ManejoEncodingUFT8.fixEncoding("mar. 9 may."));
+    }
+    @Test
+    public void CP001_TC002_OK () throws InterruptedException{
+        homePage = new HomePage(super.driver);
+        vuelosPage = new VuelosPage(super.driver);
+        homePage.navegarAPagina("https://www.rumbo.es/");
+        homePage.aceptarCookies();
+        homePage.clickVuelos();
+        Thread.sleep(1000);
+        vuelosPage.ClickSoloIda();
+        vuelosPage.EscribirOrigen();
+        vuelosPage.EscribirDestino();
+        vuelosPage.ClickDia();
+        vuelosPage.ClickPasajeros();
+        vuelosPage.AgregarPasajero();
+        vuelosPage.AgregarPasajeros();
+        Thread.sleep(1000);
+        vuelosPage.AgregarPasajeros();
+        Thread.sleep(1000);
+        vuelosPage.AgregarPasajeros();
+        Thread.sleep(1000);
+        vuelosPage.AgregarPasajeros();
+        vuelosPage.ClickBuscar();
+        Assertions.assertEquals(vuelosPage.obtenerTextoBusqueda(), ManejoEncodingUFT8.fixEncoding("mar. 9 may."));
+    }
+    @Test
+    public void CP001_TC003_NOOK () throws InterruptedException{
+        homePage = new HomePage(super.driver);
+        vuelosPage = new VuelosPage(super.driver);
+        homePage.navegarAPagina("https://www.rumbo.es/");
+        homePage.aceptarCookies();
+        homePage.clickVuelos();
+        Thread.sleep(1000);
+        vuelosPage.ClickSoloIda();
+        vuelosPage.EscribirOrigen();
+        vuelosPage.EscribirDestino();
+        vuelosPage.ClickDia();
+        vuelosPage.ClickPasajeros();
+        vuelosPage.AgregarPasajero();
+        vuelosPage.ClickBuscar();
+        vuelosPage.CambiarPestaniaResultados();
+        Assertions.assertEquals(vuelosPage.obtenerTextoBusquedaPestania(), ManejoEncodingUFT8.fixEncoding("mar. 9 may."));
     }
 }
